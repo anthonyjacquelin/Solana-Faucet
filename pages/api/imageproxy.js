adff60c100f4e9ce8c72313bbc0ebb203609b993
anthonyjacquelin <anthony.jacquelin78@gmail.com>
2022-04-01 02:05:39 +0200

    finished project

--- pages/api/imageproxy.js
+++
@@ -1,7 +0,0 @@
-export default async (req, res) => {
-  const url = decodeURIComponent(req.query.url);
-  const result = await fetch(url);
-  const body = await result.body;
-//   console.log("body: ", body);
-  body.pipe(res);
-};
